0a46e95dd151249cc25f7b96f68867079915e167
aussieint <aussieint@yandex.ru>
2023-02-05 13:18:23 +0300

    add code

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 from requests import get
 
 
-new = f'/feed/?q=flask'
+new = f'/feed/?q=django'
 with open('base.csv', mode='w', encoding='utf8') as f:
     tt = DictWriter(f, fieldnames=['date', 'title', 'link', 'text'], delimiter=';')
     tt.writeheader()
